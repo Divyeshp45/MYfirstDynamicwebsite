bf42284dfb0c2f87cf8fe67c569ac0f828b1b054
Divyesh pahurkar <84242958+Divyeshp45@users.noreply.github.com>
2022-09-29 19:58:35 +0530

    Delete MYfirstDynamicwebsite-master/portfolio directory

--- MYfirstDynamicwebsite-master/portfolio/index/admin.py
+++
@@ -1,5 +0,0 @@
-from index.models import Feedback
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(Feedback)
--- MYfirstDynamicwebsite-master/portfolio/index/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Feedback(models.Model):
-    email=models.CharField(max_length=30)
-    user_pass=models.CharField(max_length=30)
